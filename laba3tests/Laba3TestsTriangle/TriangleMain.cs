0fdf0fa16e9de08fbf2e8f371e4b2d4c7ea9acfb
karyrd <KaRylul@yandex.by>
2019-10-02 10:58:12 +0300

    added laba 3

---
+++ laba3tests/Laba3TestsTriangle/TriangleMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3TestsTriangle
+{
+    public class TriangleMain
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static bool isTriangleValid(int side1len, int side2len, int side3len)
+        {
+            if(side1len <= 0 || side2len <= 0 || side3len <= 0)
+            {
+                return false;
+            }
+            return (side1len + side2len > side3len && 
+                side2len + side3len > side1len && 
+                side1len + side3len > side2len);
+        }
+    }
+}
---
+++ laba3tests/Laba3TestsTriangleLib/TriangleTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laba3TestsTriangle;
+
+namespace Laba3TestsTriangleLib
+{
+    public class TriangleTests
+    {
+        [Test]
+        public void TriangleExists()
+        {
+            Assert.IsTrue(TriangleMain.isTriangleValid(6, 7, 10));
+        }
+        [Test]
+        public void TriangleExistsNot()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(4, 5, 11));
+        }
+        [Test]
+        public void TriangleWithZero()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(6, 7, 0));
+        }
+        [Test]
+        public void TriangleWithZeroAll()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(0, 0, 0));
+        }
+        [Test]
+        public void TriangleNegative()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(6, -2, 10));
+        }
+        [Test]
+        public void TriangleNegativeAll()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(-6, -7, -10));
+        }
+        [Test]
+        public void IsIsosceles()
+        {
+            Assert.IsTrue(TriangleMain.isTriangleValid(6, 6, 10));
+        }
+        [Test]
+        public void IsEquilateral()
+        {
+            Assert.IsTrue(TriangleMain.isTriangleValid(6, 6, 6));
+        }
+        [Test]
+        public void TriangleWithZeroAllButOne()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(0, 0, 10));
+        }
+        [Test]
+        public void TriangleNegativeAllButOne()
+        {
+            Assert.IsTrue(!TriangleMain.isTriangleValid(-6, 7, -10));
+        }
+    }
+}
